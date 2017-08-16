a8b266d4307270f5bf175db73464fdb127bfc580
Charu Mishra <Charu.Mishra@ibm.com>
2017-08-16 12:06:28 -0500

    more edits to dashboard which may not happen

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -9,6 +9,13 @@ function addCard(){
   title.innerHTML = "Title Track";
   var tagcont = document.createElement("div");
   tagcont.id= "tag-container";
+  var tagbox = document.createElement("div");
+  tagbox.id = "tags";
+  var tag = document.createElement("p");
+  tag.innerHTML = "Chill";
+  tag.id = "tag-labels";
+  tagbox.appendChild(tag);
+  tagcont.appendChild(tagbox);
   tracks.appendChild(img);
   tracks.appendChild(title);
   tracks.appendChild(tagcont);
